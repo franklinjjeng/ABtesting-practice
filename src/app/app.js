d6f2061d51d61021cdc6028fa2792aa99719ff23
Franklin Jeng <franklinjjeng@gmail.com>
2017-05-19 17:26:42 -0700

    (feat) adding cookie to remember previous color

--- src/app/app.js
+++ src/app/app.js
@@ -2,17 +2,35 @@ var blueball = 'https://img.clipartfest.com/4a1780a63f0c67b9d33a474555fa28da_blu
 var redball = 'https://img.clipartfest.com/c136f711624671ef0876f2edec9980ae_red-ball-image-red-ball-box-clipart_2400-2400.png';
 
 var ABsplit = function() {
-  var random = Math.random();
-  if (random > 0.5) {
-    $('body').append('<img style="width: 20%" src="' + redball + '">');
+  var color = $.cookie('redVSblue');
+  if (color) {
+    console.log('cookie found', color);
+    display(color);
+    return;
   } else {
-    $('body').append('<img style="width: 20%" src="' + blueball + '">');
+    console.log('no cookie found, assigning color');
+    var random = Math.random();
+    if (random > 0.5) {
+      color = 'red';
+    } else {
+      color = 'blue';
+    }
+
+    display(color);
   }
 
   return;
 }
 
+var display = function(color) {
+
+  if (color === 'red') {
+    $('body').append('<img style="width: 20%" src="' + redball + '">');
+  } else if (color === 'blue') {
+    $('body').append('<img style="width: 20%" src="' + blueball + '">');
+  }
+
+}
 
 ABsplit();
-
 
